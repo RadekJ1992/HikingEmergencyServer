862ccc38a61f2f47a91360eb7e5d36e2de2a3255
Radoslaw Jarzynka <radoslaw.jarzynka@gmail.com>
2014-10-08 17:04:44 +0200

    Na razie test - tworzenie streamów korzystających z portu szeregowego

---
+++ src/pl/rj/hikingemergency/Constants.java
@@ -0,0 +1,14 @@
+/**
+ * created on 15:15:22 8 paź 2014 by Radoslaw Jarzynka
+ * 
+ * @author Radoslaw Jarzynka
+ */
+package pl.rj.hikingemergency;
+
+public final class Constants {
+    public static String SERIAL_PORT_NAME = "/dev/tty.USB Modem Port_";
+    public static int TIMEOUT = 5000;
+    public static int BAUD_RATE = 57600;
+    public static String APPLICATION_NAME = "Hiking Emergency";
+
+}
