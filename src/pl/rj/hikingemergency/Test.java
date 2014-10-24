c8a6683b34f162dd553510e1399d2aae40ac4bb7
Radoslaw Jarzynka <radoslaw.jarzynka@gmail.com>
2014-10-24 21:14:12 +0200

    Dodanie biblioteki SMSlib do obsługi modemu gsm

--- src/pl/rj/hikingemergency/Test.java
+++ src/pl/rj/hikingemergency/Test.java
@@ -5,116 +5,60 @@
  */
 package pl.rj.hikingemergency;
 
-import gnu.io.*;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.Enumeration;
-
-public class Test {
-
-    public static void showList() {
-        Enumeration ports = CommPortIdentifier.getPortIdentifiers();
-
-        while (ports.hasMoreElements())
-            System.out.println(((CommPortIdentifier) ports.nextElement())
-                    .getName());
+import org.smslib.*;
+import org.smslib.modem.SerialModemGateway;
+public class Test
+{
+    public void doIt() throws Exception
+    {
+        OutboundNotification outboundNotification = new OutboundNotification();
+        System.out.println("Example: Send message from a serial gsm modem.");
+        System.out.println(Library.getLibraryDescription());
+        System.out.println("Version: " + Library.getLibraryVersion());
+        SerialModemGateway gateway = new SerialModemGateway("modem.com1", Constants.SERIAL_PORT_NAME, 115200, "D-Link", "DWM-156");
+        gateway.setInbound(true);
+        gateway.setOutbound(true);
+        gateway.setSimPin("0000");
+// Explicit SMSC address set is required for some modems.
+// Below is for VODAFONE GREECE - be sure to set your own!
+        gateway.setSmscNumber("+48601000310");
+        Service.getInstance().setOutboundMessageNotification(outboundNotification);
+        Service.getInstance().addGateway(gateway);
+        Service.getInstance().startService();
+        System.out.println();
+        System.out.println("Modem Information:");
+        System.out.println("  Manufacturer: " + gateway.getManufacturer());
+        System.out.println("  Model: " + gateway.getModel());
+        System.out.println("  Serial No: " + gateway.getSerialNo());
+        System.out.println("  SIM IMSI: " + gateway.getImsi());
+        System.out.println("  Signal Level: " + gateway.getSignalLevel() + " dBm");
+        System.out.println("  Battery Level: " + gateway.getBatteryLevel() + "%");
+        System.out.println();
+// Send a message synchronously.
+        OutboundMessage msg = new OutboundMessage("48605553905", "Hello from SMSLib!");
+        Service.getInstance().sendMessage(msg);
+        System.out.println(msg);
+        System.out.println("Now Sleeping - Hit <enter> to terminate.");
+        System.in.read();
+        Service.getInstance().stopService();
     }
-
-    void connect(String portName) throws Exception {
-        CommPortIdentifier portIdentifier = CommPortIdentifier
-                .getPortIdentifier(portName);
-        if (portIdentifier.isCurrentlyOwned()) {
-            System.out.println("Error: Port is currently in use");
-        } else {
-            int timeout = 2000;
-            CommPort commPort = portIdentifier.open(this.getClass().getName(),
-                    timeout);
-
-            if (commPort instanceof SerialPort) {
-                SerialPort serialPort = (SerialPort) commPort;
-                serialPort.setSerialPortParams(57600, SerialPort.DATABITS_8,
-                        SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
-
-                InputStream in = serialPort.getInputStream();
-                OutputStream out = serialPort.getOutputStream();
-
-                (new Thread(new SerialReader(in))).start();
-                (new Thread(new SerialWriter(out))).start();
-
-            } else {
-                System.out
-                        .println("Error: Only serial ports are handled by this example.");
-            }
-        }
-    }
-
-    public static class SerialReader implements Runnable {
-
-        InputStream in;
-
-        public SerialReader(InputStream in) {
-            this.in = in;
-        }
-
-        public void run() {
-            byte[] buffer = new byte[1024];
-            int len = -1;
-            try {
-                while (true/* ( len = this.in.read( buffer ) ) /*&gt; -1 */) {
-                    System.out.print(new String(buffer, 0, len));
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+    public class OutboundNotification implements IOutboundMessageNotification
+    {
+        public void process(AGateway gateway, OutboundMessage msg)
+        {
+            System.out.println("Outbound handler called from Gateway: " + gateway.getGatewayId());
+            System.out.println(msg);
         }
     }
-
-    public static class SerialWriter implements Runnable {
-
-        OutputStream out;
-
-        public SerialWriter(OutputStream out) {
-            this.out = out;
+    public static void main(String args[])
+    {
+        Test app = new Test();
+        try
+        {
+            app.doIt();
         }
-
-        public void run() {
-            try {
-                int c = 0;
-                while (true/* ( c = System.in.read() ) /*&gt; -1 */) {
-                    this.out.write(c);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-
-        try {
-            CommPortIdentifier portId = CommPortIdentifier
-                    .getPortIdentifier(Constants.SERIAL_PORT_NAME);
-            SerialPort serialPort = (SerialPort) portId.open(
-                    Constants.APPLICATION_NAME, Constants.TIMEOUT);
-            // Set serial port to 57600bps-8N1..my favourite
-            serialPort.setSerialPortParams(Constants.BAUD_RATE,
-                    SerialPort.DATABITS_8, SerialPort.STOPBITS_1,
-                    SerialPort.PARITY_NONE);
-            serialPort.setFlowControlMode(
-                    SerialPort.FLOWCONTROL_NONE);
-            OutputStream outStream = serialPort.getOutputStream();
-            InputStream inStream = serialPort.getInputStream();
-
-        } catch (NoSuchPortException e) {
-            e.printStackTrace();
-        } catch (PortInUseException e) {
-            e.printStackTrace();
-        } catch (UnsupportedCommOperationException ex) {
-            System.err.println(ex.getMessage());
-        }
-        catch (IOException e) {
+        catch (Exception e)
+        {
             e.printStackTrace();
         }
     }
